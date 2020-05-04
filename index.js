a5337e49198afa76c0d3b218ad7936219909cf77
renee112311 <renee1112311@gmail.com>
2020-05-04 10:04:04 +0800

    新增加入好友自動訊息

--- index.js
+++ index.js
@@ -16,13 +16,26 @@ const bot = linebot({
 })
 
 // 可以去看事件有啥>>linebot npm
-
+// 當加入好友時
+bot.on('follow', async (event) => {
+  let msg = ''
+  try {
+    msg = '您好，歡迎使用今明 36 小時天氣預報Line機器人，以下為使用說明：輸入「!縣市名稱」來查詢該縣市36小時內天氣預報，範例：!臺北市'
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+  event.reply(msg)
+})
 // 當收到訊息時
 bot.on('message', async (event) => {
   let msg = ''
   try {
     const data = await rp({ uri: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-66420B34-870E-4663-8957-5FD6335D5647&format=JSON', json: true })
-    msg = data.records.datasetDescription
+    if (event.message.text === '!') {
+      msg = data.records.datasetDescription
+    } else {
+      msg = '輸入錯誤'
+    }
   } catch (error) {
     msg = '發生錯誤'
   }
@@ -32,3 +45,6 @@ bot.on('message', async (event) => {
 bot.listen('/', process.env.PORT, () => {
   console.log('機器人已啟動')
 })
+
+// 您好，歡迎使用今明 36 小時天氣預報Line機器人，以下為使用說明：
+// 輸入 「!縣市名稱」來查詢該縣市36小時內天氣預報，範例：!臺北市
