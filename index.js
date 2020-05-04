b022c8b358a87a5a0c997ff00bf05f6598cb641d
renee112311 <renee1112311@gmail.com>
2020-05-04 14:17:40 +0800

    更新查詢功能

--- index.js
+++ index.js
@@ -29,10 +29,30 @@ bot.on('follow', async (event) => {
 // 當收到訊息時
 bot.on('message', async (event) => {
   let msg = ''
+  let id = 0
+  const txttrim = event.message.text.trim()
   try {
     const data = await rp({ uri: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-66420B34-870E-4663-8957-5FD6335D5647&format=JSON', json: true })
-    if (event.message.text === '!') {
-      msg = data.records.datasetDescription
+    for (const locationid in data.records.location) {
+      if (txttrim === `!${data.records.location[locationid].locationName}`) {
+        id = locationid
+      }
+      break
+    }
+    const RES = data.records.location[id]
+
+    if (txttrim.startsWith('!')) {
+      msg = `查詢${RES.locationName}的天氣預報：\n`
+      for (let i = 0; i < RES.weatherElement[0].time.length; i++) {
+        msg += `
+        ${RES.weatherElement[0].time[i].startTime}至${RES.weatherElement[0].time[i].endTime}\n
+        天氣現象：${RES.weatherElement[0].time[i].parameter.parameterName}\n
+        降雨機率：${RES.weatherElement[1].time[i].parameter.parameterName}%\n
+        最低溫：${RES.weatherElement[2].time[i].parameter.parameterName}℃\n
+        最高溫：${RES.weatherElement[4].time[i].parameter.parameterName}℃\n
+        舒適度：${RES.weatherElement[3].time[i].parameter.parameterName}\n
+        `
+      }
     } else {
       msg = '輸入錯誤'
     }
@@ -45,6 +65,3 @@ bot.on('message', async (event) => {
 bot.listen('/', process.env.PORT, () => {
   console.log('機器人已啟動')
 })
-
-// 您好，歡迎使用今明 36 小時天氣預報Line機器人，以下為使用說明：
-// 輸入 「!縣市名稱」來查詢該縣市36小時內天氣預報，範例：!臺北市
